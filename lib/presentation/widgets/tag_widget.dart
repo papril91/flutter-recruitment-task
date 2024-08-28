811f42760dd1b047a6ffbd5c48cddec9d97e22d1
pkwiecien <patryk.kwiecien@deviniti.com>
2024-08-28 21:45:55 +0200

    Add border for selected tag widget

--- lib/presentation/widgets/tag_widget.dart
+++ lib/presentation/widgets/tag_widget.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_recruitment_task/models/products_page.dart';
 
 class TagWidget extends StatelessWidget {
-  const TagWidget(this.tag, {super.key});
+  const TagWidget(this.tag, {super.key, this.isSelected = false});
 
   final Tag tag;
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,14 @@ class TagWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4),
       child: Chip(
+        elevation: isSelected ? 4.0 : 2.0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: isSelected ? Colors.black87 : Colors.transparent,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
         backgroundColor: hexToColor(tag.color),
         label: Text(tag.label),
       ),
